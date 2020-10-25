f98e8f2560a65cc2d5f2b4332161a00773cfa794
Lourdes Roashan <root.roashan@gmail.com>
2020-10-25 00:17:22 -0300

    passing data from App to Table component using props

--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -2,32 +2,43 @@ import React, {Component} from 'react'
 
 class Table extends Component {
   render() {
+    
+    const {
+        characterData
+    } = this.props;
+    const TableHeader = () => {
+        return (
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+        )
+    };
+
+    const TableBody = (props) => {
+        const rows = props.characterData.map(data => {
+            return (
+                <tr>
+                    <td>{data.name}</td>
+                    <td>{data.job}</td>
+                </tr>
+            )
+        });
+
+        return (
+          <tbody>
+              {rows}
+          </tbody>
+        )
+      };
+
+
     return (
       <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Job</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-          </tr>
-          <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-          </tr>
-          <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-          </tr>
-          <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-          </tr>
-        </tbody>
+          <TableHeader/>
+          <TableBody characterData={characterData}/>
       </table>
     )
   }
